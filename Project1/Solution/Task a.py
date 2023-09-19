4ede824713db0fa9d8e1eac4bb2eccd852c17fa4
inelaa <144776311+inelaa@users.noreply.github.com>
2023-09-19 14:38:06 +0200

    Edited task a to perform the plotting with the new plotting class.

--- Project1/Solution/Task a.py
+++ Project1/Solution/Task a.py
@@ -6,6 +6,7 @@ plt.style.use('Solarize_Light2')
 
 from Franke_function import FrankeFunction
 from LinRegression import LinRegression
+from Plotting import Plotting
 from setup import save_fig, data_path
 
 if __name__ == '__main__':
@@ -18,60 +19,48 @@ if __name__ == '__main__':
 
     z = FrankeFunction(x, y)
 
+    MSE_test_scores = []
+    R2_test_scores = []
+    beta_parameters = []
+    polydegree = 5
 
-    OLS_regression = LinRegression(5, x, y, z)
-    print(np.shape(OLS_regression.X))
-    print(np.shape(OLS_regression.y))
+    for polyorder in range(1,polydegree+1):
 
-    OLS_regression.split_data(1/5)
+        OLS_regression = LinRegression(polyorder, x, y, z) #create class
+        OLS_regression.split_data(1/5) # perform split of data
 
-    print(f'Split performed: {OLS_regression.splitted}')
+        print(f'Split performed: {OLS_regression.splitted}')
 
+        OLS_regression.scale(scaling_method='StandardScaling')
+        print(f'Scaling performed: {OLS_regression.scaled}\n'
+               f'Scaling methode: {OLS_regression.scaling_method}')
 
-    OLS_regression.scale(scaling_method='StandardScaling')
-    print(f'Scaling performed: {OLS_regression.scaled}\n'
-           f'Scaling methode: {OLS_regression.scaling_method}')
+        OLS_regression.train_model(train_on_scaled=True, regression_method='OLS')
+        print(f'The optimal parametres are: {OLS_regression.beta}')
+        beta_parameters.append(OLS_regression.beta)
 
+        OLS_regression.predict_training()
 
-    OLS_regression.train_model(train_on_scaled=True, regression_method='OLS')
-    print(f'The optimal parametres are: {OLS_regression.beta}')
+        OLS_regression.predict_test()
 
-    OLS_regression.predict_training()
+        MSE_training = OLS_regression.MSE(OLS_regression.y_train, OLS_regression.y_pred_train)
+        MSE_test = OLS_regression.MSE(OLS_regression.y_test, OLS_regression.y_pred_test)
 
-    OLS_regression.predict_test()
+        MSE_test_scores.append(MSE_test)
 
+        # The mean squared error
+        print(f'Mean squared error training: {MSE_training:.4f}')
+        print(f'Mean squared error test: {MSE_test:.4f}')
 
-    # The mean squared error
-    print(f'Mean squared error training: {OLS_regression.MSE(OLS_regression.y_train, OLS_regression.y_pred_train):.4f}')
-    print(f'Mean squared error test: {OLS_regression.MSE(OLS_regression.y_test, OLS_regression.y_pred_test):.4f}')
+        R2_training = OLS_regression.R_squared(OLS_regression.y_train, OLS_regression.y_pred_train)
+        R2_test = OLS_regression.R_squared(OLS_regression.y_test, OLS_regression.y_pred_test)
 
-    from sklearn.metrics import r2_score
-    print(f'R^2 training: {OLS_regression.R_squared(OLS_regression.y_train, OLS_regression.y_pred_train):.4f}')
-    print(f'R^2 test: {OLS_regression.R_squared(OLS_regression.y_test, OLS_regression.y_pred_test):.4f}')
+        R2_test_scores.append(R2_test)
+        print(f'R^2 training: {R2_training:.4f}')
+        print(f'R^2 test: {R2_test:.4f}')
 
-
-
-    """
-        # Plot the surface.
-    
-        from mpl_toolkits.mplot3d import Axes3D
-        import matplotlib.pyplot as plt
-        from matplotlib import cm
-        from matplotlib.ticker import LinearLocator, FormatStrFormatter
-    
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-    
-        surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
-                               linewidth=0, antialiased=False)
-    
-        # Customize the z axis.
-        ax.set_zlim(-0.10, 1.40)
-        ax.zaxis.set_major_locator(LinearLocator(10))
-        ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-    
-        # Add a color bar which maps values to colors.
-        fig.colorbar(surf, shrink=0.5, aspect=5)
-        plt.show()
-    """
-
+    # Plotting results of task 1a OLS regression
+    plots_task_1a = Plotting(5, MSE_test_scores, R2_test_scores, beta_parameters)
+    plots_task_1a.plot_MSE_scores()
+    plots_task_1a.plot_R2_scores()
+    plots_task_1a.plot_betaparams_polynomial_order()
