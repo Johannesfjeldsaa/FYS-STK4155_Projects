1eefd41e9d9b12e426945ea22561092510aa9856
inelaa <144776311+inelaa@users.noreply.github.com>
2023-09-23 16:47:22 +0200

    Creating class to perform sampling such as cross validation (+bootstrap?)

---
+++ Project1/Solution/Resampling.py
@@ -0,0 +1,23 @@
+"""
+Class to provide resampling techniques.
+"""
+
+
+class Resampling:
+
+    def __init__(self,k_folds):
+
+
+    def cross_validation(self):
+        """
+        Method that will resample the data in k-folds
+
+        :return:
+        """
+
+
+    def bootstrapping(self):
+        """
+        Method to reshuffle/split the data with bootstrapping.
+        :return:
+        """
