a73ad0f48da6e296ff05651766c02b4b41ea345a
inelaa <144776311+inelaa@users.noreply.github.com>
2023-09-24 19:12:02 +0200

    starting the resampling class

--- Project1/Solution/Resampling.py
+++ Project1/Solution/Resampling.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 """
 Class to provide resampling techniques.
 """
@@ -5,16 +7,30 @@ Class to provide resampling techniques.
 
 class Resampling:
 
-    def __init__(self,k_folds):
+    def __init__(self,k_folds, x, y):
 
+        self.x = x
+        self.y = y
+        self.k = k_folds
+        self.n = len(self.x)
 
     def cross_validation(self):
         """
-        Method that will resample the data in k-folds
+        Method that will resample the data in k-fold groups
 
         :return:
         """
+        np.random.shuffle(self.x)
 
+        # Split the data into k equal groups
+        group_size = self.n // self.k  # divide with integral result (discard remainder)
+        groups = [self.x[i:i + group_size] for i in range(0, self.n, group_size)]
+
+        return groups
+
+    def scikit_cross_validation(self):
+
+        pass
 
     def bootstrapping(self):
         """
