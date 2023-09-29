233c4980f0bf15254329e1ddc08f0824499d9251
inelaa <144776311+inelaa@users.noreply.github.com>
2023-09-29 15:40:32 +0200

    added plot comparing own code to scikit

--- Project1/Solution/oppg_f.py
+++ Project1/Solution/oppg_f.py
@@ -5,13 +5,12 @@ import matplotlib.pyplot as plt
 import random
 
 
-polydegree = 13
+polydegree = 14
 x = np.linspace(0,1,100)
 y = np.linspace(0,1,100)
 z = FrankeFunction(x,y)
-k = 10
-seed = 1917
-np.random.seed(seed)
+k = 5
+np.random.seed(1917)
 
 # store the results for each model complexity
 optimal_beta_cross_validation = []
@@ -26,7 +25,7 @@ for polydegree in range(polydegree+1):
 
         cross_validation = LinRegression(polydegree, x, y, z)  # this needs to be remade each time
         mean_B_parameters, mean_MSE_test, mean_MSE_train, mean_R2_test, mean_R2_train \
-            = cross_validation.cross_validation_train_model(k, seed)
+            = cross_validation.cross_validation_train_model(k)
 
         optimal_beta_cross_validation.append(mean_B_parameters)
         MSE_test.append(mean_MSE_test)
@@ -34,7 +33,30 @@ for polydegree in range(polydegree+1):
         R2_test.append(mean_R2_test)
         R2_train.append(mean_R2_train)
 
+"""
+using scikit_learn library
+"""
 
+mean_scikit_MSE_cv = []  # antar dette er test mse?
+
+for polydegree in range(polydegree+1):
+
+    if polydegree != 0:
+
+        scikit_cv = LinRegression(polydegree,x,y,z)
+        mean_scikit_MSE_cv.append(scikit_cv.scikit_cross_validation_train_model(k))
+
+"""
+Compare own code to scikit code (can add in bootstrap later here)
+"""
+
+plt.figure()
+plt.xlabel('model complexity')
+plt.ylabel('MSE score')
+plt.plot(range(1,polydegree + 1), MSE_test, 'r', label='Own code')
+plt.plot(range(1,polydegree + 1), mean_scikit_MSE_cv, 'b', label='Scikit')
+plt.legend()
+plt.show()
 
 fig, ax1 = plt.subplots(figsize=(9, 6))
 ax2 = ax1.twinx()
