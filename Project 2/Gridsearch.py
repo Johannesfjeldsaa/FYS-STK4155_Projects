d735c157d11b148b2802f77f2d326d5b0fe03b41
inelaa <144776311+inelaa@users.noreply.github.com>
2023-10-22 13:38:38 +0200

    improving

--- Project 2/Gridsearch.py
+++ Project 2/Gridsearch.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from GD_and_SGD_analytical import GD, SGD
 from GD_and_SGD_AD import AD_GD, AD_SGD
-
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 """
 Make a class that will find min MSE scores (+R2 scores) for wat you want
@@ -14,18 +15,42 @@ inspired by Daniel Haas code
 
 cm = 1/2.54
 
-## må utvides så vi kan kjøre med flere metoder.
 def GridSearch_LinReg_epochs_batchsize(p, x, y, learning_rate, batch_sizes, n_epochs, lmbda=0,
                                        regression_method='OLS', approach='approach',
                                        optimization=None, momentum=0.0, k=5,
-                                       rho1=None, rho2=None, plot_grid=True):
+                                       rho1=0.9, rho2=0.99, plot_grid=True):
+    """
+    Function for calculating mse and r2 scores using crossvalidation as training method.
+    Can retrieve for different paths: OLS or Ridge, analytical or automatic differentiation and
+    optimization methods no momentum, momentum, RMSprop, adagrad and adam.
+
+    :param p: the order of polynomial fit for design matrix
+    :param x: initail input data
+    :param y: initail input data y(x)
+    :param learning_rate: initial guess of step size
+    :param batch_sizes: an array to test which one is performing better
+    :param n_epochs: an array to test what is optimal epoch size
+    :param lmbda: lmbda value for ridge regression, if not change lmbda = 0
+    :param regression_method: chosen regression method: OLS or Ridge
+    :param approach: Choose between analytical and automatic differentiation approach
+    :param optimization: method used for optimization:no momentum,momentum,adagrad,RMSprop or adam
+    :param momentum: momentum parameter, set to 0 if not changed
+    :param k: number of crossvalidation kfolds
+    :param rho1: parameter for momentum methods, usually set to 0.9
+    :param rho2: parametor for momemntum methods, usually set to 0.99
+    :param plot_grid: set to True, plot a heatmap for visualising results
+    :return: mse scores and r2 scores for each combination of epoch and batchsize
+    """
     mse_values = np.zeros((len(batch_sizes), len(n_epochs)))
     r2_values = np.zeros((len(batch_sizes), len(n_epochs)))
 
+    linreg = LinRegression(p, x, y)
+
     for i, M in enumerate(batch_sizes):
         for j, epochs in enumerate(n_epochs):
-            print(f"Computing batch size={M} and num. epochs={epochs}.")
-            linreg = LinRegression(p, x, y)
+            #print(f"Computing batch size={M} and num. epochs={epochs}.")
+
+
             if approach == 'analytical':
                 beta = SGD(linreg.X, linreg.y, batch_sizes[i], n_epochs[j], learning_rate,
                            momentum=momentum, tol=10**-8, regression_method=regression_method,
