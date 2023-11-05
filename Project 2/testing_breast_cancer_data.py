54c1bc1900c75eabc630e6c607a54ec124b42e47
inelaa <144776311+inelaa@users.noreply.github.com>
2023-11-05 22:09:30 +0100

    trying to find something to compare classification with NN and logistic regression

--- Project 2/testing_breast_cancer_data.py
+++ Project 2/testing_breast_cancer_data.py
@@ -68,6 +68,7 @@ ax[0].set_xlabel("Feature magnitude")
 ax[0].set_ylabel("Frequency")
 ax[0].legend(["Malignant", "Benign"], loc ="best")
 fig.tight_layout()
+plt.title('Histograms derived from Logistic regression')
 plt.show()
 
 import seaborn as sns
@@ -76,6 +77,7 @@ correlation_matrix = cancerpd.corr().round(1)
 # annot = True to print the values inside the square
 plt.figure(figsize=(15,8))
 sns.heatmap(data=correlation_matrix, annot=True)
+plt.title('Correlation matrix for Logistic Regression')
 plt.show()
 
 
@@ -86,13 +88,13 @@ our try on breast cancer
 
 # Load the data
 cancer = load_breast_cancer()
-X = cancer.data
+X_orig = cancer.data
 target_true = cancer.target
 target_true = target_true.reshape(target_true.shape[0], 1)
 
 """
 #splitting
-X_train, X_test, y_train, y_test = train_test_split(X, target) #random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X_orig, target_true, random_state=0)
 
 #now scale the data
 scaler = StandardScaler()
@@ -111,7 +113,7 @@ learning_rate=0.1
 #training NN with breastcancer data
 NN_output_cancer = []
 
-for X_row, target_row in zip(X, target_true):
+for X_row, target_row in zip(X_orig, target_true):
 
     X = jnp.array([X_row])
     target = jnp.array([target_row])
@@ -129,76 +131,18 @@ for X_row, target_row in zip(X, target_true):
     NN_output_cancer.append(nn.output_layer.output)
 
 
-print(NN_output_cancer)
-print(len(target_true))
-
-print(len(NN_output_cancer))
-
 
 
 """
 # Now i want to find the target output from NN to set this for plotting and comparing to logreg
 # follow rest of log reg example after this has been shown.
-malignant = cancer.data[cancer.target == 0]
-benign = cancer.data[cancer.target == 1]
 """
 
-"""
-starting to get out what i want
-"""
+
 
 # Making a data frame
-#cancerpd = pd.DataFrame(cancer.data, columns=cancer.feature_names)
+cancerpd = pd.DataFrame(cancer.data, columns=cancer.feature_names)
 
-
-
-
-"""
-#%%
-np.random.seed(1234)
-
-# Create design matrix
-X_orig = jnp.array([[0, 0],
-               [0, 1],
-               [1, 0],
-               [1, 1]])
-
-
-# The XOR gate
-target_XOR = jnp.array( [ 0, 1 ,1, 0])
-# The OR gate
-target_OR = jnp.array( [ 0, 1 ,1, 1])
-# The AND gate
-target_AND = jnp.array( [ 0, 0 ,0, 1])
-
-n_hidden_layers = 1
-n_hidden_nodes = 2
-n_outputs = 1
-learning_rate=0.1
-
-# AND gate
-NN_output_AND = []
-
-for X_row, target_row in zip(X_orig, target_AND):
-
-    X = jnp.array([X_row])
-    target = jnp.array([target_row])
-
-    ffnn = Neural_Network(X, target, n_hidden_layers, n_hidden_nodes, n_outputs,
-                      learning_rate, activation_function='sigmoid',
-                      classification_problem=True)
-
-    ffnn.feed_forward()
-
-
-    ffnn.feed_backward()
-    ffnn.train()
-
-    NN_output_AND.append(ffnn.output_layer.output)
-
-
-print(NN_output_AND)
-"""
 
 def accuracy_test(y_true, y_pred):
     # y_true is the true labels of the data
@@ -218,9 +162,44 @@ def accuracy_test(y_true, y_pred):
     # Return the accuracy as a percentage
     return accuracy * 100
 
-## test your accuracy:
-#accuracy_AND_gate = accuracy_test(target_AND, NN_output_AND)
-#print(accuracy_AND_gate)
 
 accuracy_breast_cancer = accuracy_test(target_true, NN_output_cancer)
 print(accuracy_breast_cancer)
+
+# Making a data frame
+cancerpd = pd.DataFrame(cancer.data, columns=cancer.feature_names)
+
+
+fig, axes = plt.subplots(15,2,figsize=(10,20))
+
+NN_output_cancer = jnp.array(NN_output_cancer).ravel()   # think this is correct format
+
+malignant = X_orig[NN_output_cancer == 0]
+print(malignant)
+print(malignant.shape)
+benign = X_orig[NN_output_cancer == 1]
+print(malignant)
+print(malignant.shape)
+ax = axes.ravel()
+
+for i in range(30):
+    _, bins = np.histogram(X_orig[:,i], bins =50)
+    ax[i].hist(malignant[:,i], bins = bins, alpha = 0.5)
+    ax[i].hist(benign[:,i], bins = bins, alpha = 0.5)
+    ax[i].set_title(cancer.feature_names[i])
+    ax[i].set_yticks(())
+ax[0].set_xlabel("Feature magnitude")
+ax[0].set_ylabel("Frequency")
+ax[0].legend(["Malignant", "Benign"], loc ="best")
+fig.tight_layout()
+plt.title('Histograms derived from Neural Network')
+plt.show()
+
+import seaborn as sns
+correlation_matrix = cancerpd.corr().round(1)
+# use the heatmap function from seaborn to plot the correlation matrix
+# annot = True to print the values inside the square
+plt.figure(figsize=(15,8))
+sns.heatmap(data=correlation_matrix, annot=True)
+plt.title('Correlation matrix for Neural Network')
+plt.show()
